fff9ec6cbe2dde33009552dea5a6262957b99841
Arman Amini <armanwithamini@gmail.com>
2024-06-30 06:29:27 -0700

    add searching functionality

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -4,21 +4,47 @@ import { ProductService } from '../product.service';
 import { MatCardModule } from '@angular/material/card';
 import { CurrencyPipe } from '@angular/common';
 import { Router } from '@angular/router';
+import { CartService } from '../../cart/cart.service';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [MatCardModule, CurrencyPipe],
+  imports: [MatCardModule, CurrencyPipe, MatSnackBarModule, MatInputModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService,
+    private snackbar: MatSnackBar
+  ) {}
   ngOnInit(): void {
     this.productService.getProducts().subscribe((data) => {
       this.products = data;
     });
   }
+  addToCart(product: Product) {
+    this.cartService.addToCart(product).subscribe({
+      next: () => {
+        this.snackbar.open('Added to cart', '', {
+          duration: 2000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+        });
+      },
+      error: (error) => {
+        this.snackbar.open('Failed to add to cart', '', {
+          duration: 2000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+        });
+      },
+    });
+  }
+  applyFiter(event: Event): void {}
 }
